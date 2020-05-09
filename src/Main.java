1f759c797d5366f65b742372fe3b669a627b9c0e
Bruno Coser <coser_bruno@hotmail.com>
2020-05-09 12:41:50 +0100

    Refactor the method and also add nextLine in the scanner statement on the customer menu

--- src/Main.java
+++ src/Main.java
@@ -15,19 +15,19 @@ public class Main {
 
 
         Scanner myScanner = new Scanner(System.in);
-        String option = "";
+        String option;
 
         do {
-            System.out.println( "-------------------- MENU --------------------\n"
-                               +"|                                            |\n"
-                               +"|   [1] Employee                             |\n"
-                               +"|   [2] Customer                             |\n"
-                               +"|   [0] Exit                                 |\n"
-                               +"|                                            |\n"
-                               +"--------Please, choose a valid option--------");
+            System.out.println("-------------------- MENU --------------------\n"
+                    + "|                                            |\n"
+                    + "|   [1] Employee                             |\n"
+                    + "|   [2] Customer                             |\n"
+                    + "|   [0] Exit                                 |\n"
+                    + "|                                            |\n"
+                    + "--------Please, choose a valid option--------");
             option = myScanner.nextLine();
 
-            switch (option){
+            switch (option) {
                 case "0":
                     System.out.println("Exit");
                     System.exit(0);
@@ -42,7 +42,7 @@ public class Main {
                     System.out.println("It's an invalid option");
                     break;
             }
-        }while (!option.equals("0"));
+        } while (true);
     }
 
     public static void menuEmployee(ArrayList<Title> listTitles, ArrayList<Customer> listCustomers) throws IOException {
@@ -54,17 +54,17 @@ public class Main {
 
         do {
             System.out.println("---------------------- MENU ----------------------\n"
-                             + "|                                                |\n"
-                             + "|   [1] Search for Titles                        |\n"
-                             + "|   [2] New Title                                |\n"
-                             + "|   [3] Search for Customers                     |\n"
-                             + "|   [4] New Customer                             |\n"
-                             + "|   [5] Update Customers and Subscription Plan   |\n"
-                             + "|   [0] Return to main menu                      |\n"
-                             + "|                                                |\n"
-                             + "----------Please, choose a valid option----------");
+                    + "|                                                |\n"
+                    + "|   [1] Search for Titles                        |\n"
+                    + "|   [2] New Title                                |\n"
+                    + "|   [3] Search for Customers                     |\n"
+                    + "|   [4] New Customer                             |\n"
+                    + "|   [5] Update Customers and Subscription Plan   |\n"
+                    + "|   [0] Return to main menu                      |\n"
+                    + "|                                                |\n"
+                    + "----------Please, choose a valid option----------");
             option = myScanner.nextLine();
-            switch (option){
+            switch (option) {
                 case "0":
                     System.out.println("Return");
                     break;
@@ -89,24 +89,25 @@ public class Main {
             }
         } while (!option.equals("0"));
     }
-    public static void  menuCustomer(ArrayList<Title> listTitles, ArrayList<Customer> listCustomers) {
+
+    public static void menuCustomer(ArrayList<Title> listTitles, ArrayList<Customer> listCustomers) {
 
         Functionality functionality = new Functionality();
 
         Scanner myScanner = new Scanner(System.in);
-        String option ="";
+        String option = "";
 
-        do{
+        do {
             System.out.println("---------------------- MENU ----------------------\n"
-                             + "|                                                |\n"
-                             + "|   [1] Search for Titles                        |\n"
-                             + "|   [2] Register a rent                          |\n"
-                             + "|   [3] Register a return                        |\n"
-                             + "|   [0] Return to main menu                      |\n"
-                             + "|                                                |\n"
-                             + "----------Please, choose a valid option----------");
-            option = myScanner.next();
-            switch (option){
+                    + "|                                                |\n"
+                    + "|   [1] Search for Titles                        |\n"
+                    + "|   [2] Register a rent                          |\n"
+                    + "|   [3] Register a return                        |\n"
+                    + "|   [0] Return to main menu                      |\n"
+                    + "|                                                |\n"
+                    + "----------Please, choose a valid option----------");
+            option = myScanner.nextLine();
+            switch (option) {
                 case "0":
                     System.out.println("Return");
                     break;
