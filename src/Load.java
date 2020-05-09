428b64c80432831aa3753fbf4d51a993800042d7
Bruno Coser <coser_bruno@hotmail.com>
2020-05-09 15:28:36 +0100

    Renamed Load

--- src/Load.java
+++ src/Load.java
@@ -8,7 +8,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
-public class load {
+public class Load {
 
     ArrayList<Customer> M_listCustomers = new ArrayList<Customer>();
     static ArrayList<Title> M_listTitles = new ArrayList<Title>();
--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        load load = new load();
+        Load load = new Load();
 
         load.loadCustomers();
         load.loadTitles();
